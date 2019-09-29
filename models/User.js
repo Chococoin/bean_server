ac2e04fdebdb2a2921ac2ea0d55c16b3eda1c7f2
Germán Lugo <german.lugo@bitnibs.com>
2019-09-29 15:58:09 +0200

    Add Token field in Schema.

--- models/User.js
+++ models/User.js
@@ -22,6 +22,10 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true
   },
+  SBToken: {
+    type: String,
+    required: false
+  },
   date: {
     type: Date,
     default: Date.now
